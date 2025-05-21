44d4605ed848aa57b55499a6ecc280e53af2a4b8
kaiocheroso <kaiogalvin17@gmail.com>
2025-05-20 22:16:57 -0300

    Update maquinas.java

--- SA/src/SA/maquinas.java
+++ SA/src/SA/maquinas.java
@@ -1,46 +1,55 @@
-package SA;
+package sa;
 import java.util.*;
-public class maquinas {
-	static Scanner entrada = new Scanner(System.in);
-	static ArrayList<Integer> maquinas = new ArrayList<>();
-	static ArrayList<String> setores = new ArrayList<>();
-	static ArrayList<Integer> pecas = new ArrayList<>();
-	
-	public static void main(String[] args) {
-		System.out.println("Digite o numeros de setores");
-		int valor = entrada.nextInt();
-		int i = 0;
-		while (i < valor) {
-			entrada.nextLine();
-			System.out.println("Digite o nome do setor: ");
-			String setor = entrada.nextLine();
-			setores.add(setor);
-			i++;
+public class main {
+	public static void main(String [] args) {
+		ArrayList<Integer> produçao = new ArrayList<>();
+		ArrayList<Integer> setorProduçao = new ArrayList<>();
+		ArrayList<String> diasSemana = new ArrayList<>();
+		diasSemana.addAll(Arrays.asList("Segunda: ", "Terça: ","Quarta: ","Quinta: ","Sexta: "));
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o numero de setores: ");
+		int Setores = entrada.nextInt();
+		System.out.println("Digite o numero de maquinas por setor: ");
+		int maquinas = entrada.nextInt();
+		int produtoSetores = 0;
+		int valor = 0;
+		int [][] matrix = new int[maquinas*Setores][5]; 
+		
+		for(int i = 0; i < Setores;i++) {
+			System.out.println("Setor "+(i+1));
+			for(int j = 0;j < maquinas; j++) {
+				System.out.println("Produção maquina "+(j+1));
+				for(int a = 0; a < 5;a++) {
+					System.out.print(diasSemana.get(a));
+					int produto = entrada.nextInt();
+					valor += produto;
+					matrix[j][a] = produto; 
+				}
+				produçao.add(valor);
+				
+				produtoSetores += valor;
+				valor = 0;
+			}
+			setorProduçao.add(produtoSetores);
+			produtoSetores = 0;
 		}
 		
-			
+		for(int t = 0; t < produçao.size();t++) {
+			System.out.println("Maquina "+(t+1)+" produção " + produçao.get(t));
+		}
+		
+		for(int n = 0; n < setorProduçao.size();n++) {
+			System.out.println("Produção do Setor "+(n+1));
+			System.out.println("Produção: "+(setorProduçao.get(n)));
+		}
+		
+		for (int b = 0; b < (maquinas*Setores);b++) {
+			for(int c = 0; c < 5;c++) {
+				System.out.println("Maquina "+(b+1)+" produção " + matrix[b][c]);
+			}
+		
+		}
 		
 	}
-	//public static void main(String[] args) {
-		//int[][] matrix = int[maquinas.size()][5];
-		//System.out.println("Digite o nome do setor: ");
-		//String setor = entrada.nextLine();
-		//setores.add(setor);
-		//for (int i = 0; i < setores.size();i++) {
-		//	System.out.println("Setor " +setores.get(i));
-		//	for(int j = 0; j < maquinas.size();j++) {
-		//		System.out.prinltn("Maquina "+(j+1));
-		//		for
-		//	}
-		//}
-		
-			
-		
-	}
-	
-
-	//public static void main(String[] args) {
-	//	Scanner entrada = new Scanner(System.in);
-	//}
-
-//}
+}
